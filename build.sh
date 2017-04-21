28429c607cefe6c86db084a49dcdfc9534e47c6a
Vikram Pawar <junoon.53@gmail.com>
2017-04-21 19:56:46 -0400

    added main.cpp and basic build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+cd source
+g++ main.cpp -o memo
+cd ../
+mkdir out
+mv source/memo out/
+echo "done"
---
+++ source/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Memo : A Data Caching Service
+ * Team Awesome
+ * Vikram Anirudha Vinithra Shruthi
+ *
+ * 
+   m                                                                         
+ mm#mm   mmm    mmm   mmmmm   mmm  m     m  mmm    mmm    mmm   mmmmm   mmm  
+   #    #"  #  "   #  # # #  "   # "m m m" #"  #  #   "  #" "#  # # #  #"  # 
+   #    #""""  m"""#  # # #  m"""#  #m#m#  #""""   """m  #   #  # # #  #"""" 
+   "mm  "#mm"  "mm"#  # # #  "mm"#   # #   "#mm"  "mmm"  "#m#"  # # #  "#mm" 
+ *
+ */
+
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
