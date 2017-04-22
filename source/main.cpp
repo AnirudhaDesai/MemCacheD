bb5b6d0ad33b0761488a5a7a305d60bfb8a95145
Vikram Pawar <junoon.53@gmail.com>
2017-04-22 19:08:20 -0400

    added command line flags

--- source/main.cpp
+++ source/main.cpp
@@ -8,20 +8,30 @@
 
 /*
    m                                                                         
- mm#mm   mmm    mmm   mmmmm   mmm  m     m  mmm    mmm    mmm   mmmmm   mmm  
-   #    #"  #  "   #  # # #  "   # "m m m" #"  #  #   "  #" "#  # # #  #"  # 
-   #    #""""  m"""#  # # #  m"""#  #m#m#  #""""   """m  #   #  # # #  #"""" 
-   "mm  "#mm"  "mm"#  # # #  "mm"#   # #   "#mm"  "mmm"  "#m#"  # # #  "#mm" 
+   mm#mm   mmm    mmm   mmmmm   mmm  m     m  mmm    mmm    mmm   mmmmm   mmm  
+#    #"  #  "   #  # # #  "   # "m m m" #"  #  #   "  #" "#  # # #  #"  # 
+#    #""""  m"""#  # # #  m"""#  #m#m#  #""""   """m  #   #  # # #  #"""" 
+"mm  "#mm"  "mm"#  # # #  "mm"#   # #   "#mm"  "mmm"  "#m#"  # # #  "#mm" 
  *
  */
 
 
 #include <iostream>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "mmapheap.h"
 #include "command_parser.h"
 
 const auto DefaultHeapSize = 2 * 1024UL * 1024 * 1024;
+
+typedef enum {
+    LRU=0,
+    RANDOM,
+    LANDLORD
+} ALG_T;
+
+ALG_T cache_replacement_algorithm = LRU;
 
 MmapHeap* heap = new MmapHeap(DefaultHeapSize);  
 
@@ -29,28 +39,54 @@ int main(int argc, char *argv[])
 {
     // parse command line args
 
-    for(int i=1;i<argc;i++)
+    static int lru_flag = 0;
+    static int rnd_flag = 0;
+    static int lndlrd_flag = 0;
+
+    static struct option long_options[] =
     {
-        printf("%s\n",argv[i]);
+        {"lru",      no_argument, &lru_flag, 1},
+        {"random",   no_argument, &rnd_flag, 1},
+        {"landlord", no_argument, &lndlrd_flag, 1}
+    };
+
+    int c;
+    int option_index = 0;
+    c = getopt_long (argc, argv, "", long_options, &option_index);
+
+    // set cache replacement algorithm 
+    //
+    if(lru_flag)
+    {
+        cache_replacement_algorithm = LRU;
+    }
+    else if(rnd_flag)
+    {
+        cache_replacement_algorithm = RANDOM;
+    }
+    else if(lndlrd_flag)
+    {
+        cache_replacement_algorithm = LANDLORD;
     }
 
+    //std::cout << "cache replacement algorithm :" << cache_replacement_algorithm << std::endl;
     //std::cout << "heap size " << (heap->getSize())/1024/1024 << std::endl;
     char* cmd_str;
     size_t cmd_len;
 
     //while(1)
     //{
-        // listen on port 
-        //
-        // read command
-        cmd_len = 50;
-        cmd_str = (char*)malloc(cmd_len);
-        sprintf(cmd_str,"set key 0 900 5\r\nvalue");
+    // listen on port 
+    //
+    // read command
+    cmd_len = 50;
+    cmd_str = (char*)malloc(cmd_len);
+    sprintf(cmd_str,"set key 0 900 5\r\nvalue");
 
-        // parse command
-        parse_command(cmd_str,cmd_len);
+    // parse command
+    parse_command(cmd_str,cmd_len);
 
-        free(cmd_str);
+    free(cmd_str);
     //}
     //
 
