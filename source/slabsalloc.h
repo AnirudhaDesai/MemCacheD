3c2db0593dcb2738022876d56e5d76dd88f37080
Shruthi Yashavanth <syashavanth@umass.edu>
2017-04-24 12:35:31 -0400

    memory allocation code

---
+++ source/slabsalloc.h
@@ -0,0 +1,98 @@
+#ifndef SLABSALLOC_H
+#define SLABSALLOC_H
+
+#include <cassert>
+#include <functional>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <new>
+#include <mutex>
+#include <sys/mman.h>
+#include <stdint.h>
+
+using namespace std;
+
+class Header {
+ public:
+  char key[251];
+  size_t data_size;  
+  uint16 flags;
+  int32 expiration_time;
+
+  Header * prev;
+  Header * next;
+};
+
+template <class SourceHeap>
+class SlabsAlloc : public SourceHeap {
+public:
+  SlabsAlloc()
+    : allocatedObjects (nullptr),
+      requested (0),
+      allocated (0),
+      maxRequested (0),
+      maxAllocated (0)
+  {
+    for (auto& f : freedObjects) {
+      f = nullptr;
+    }
+    for (auto& f : allocatedObjects) {
+      f = nullptr;
+    }
+  }
+
+  ~StatsAlloc()
+  {
+  }
+  
+  enum { Alignment = 16 };
+  
+  void * malloc(size_t sz);
+  void free(void * ptr);
+  size_t getSize(void * p);
+
+  // number of bytes currently allocated  
+  size_t bytesAllocated();
+  
+  // max number of bytes allocated  
+  size_t maxBytesAllocated();
+
+  // number of bytes *requested*
+  // (e.g., malloc(4) might result in an allocation of 8; 4 = bytes requested, 8 = bytes allocated)
+  size_t bytesRequested();
+  
+  // max number of bytes *requested*
+  size_t maxBytesRequested();
+
+  void walk(const std::function< void(Header *) >& f); 
+
+  // Return maximum size of object for a given size class.
+  static size_t getSizeFromClass(int index);
+  
+  // Return the size class for a given size.
+  static int getSizeClass(size_t sz);
+
+private:
+
+  recursive_mutex heapLock;
+  
+  size_t allocated;
+  size_t requested;
+  size_t maxAllocated;
+  size_t maxRequested;
+  
+  Header * head_AllocatedObjects[23];
+  Header * head_FreedObjects[23];
+  Header * tail_AllocatedObjects[23];
+  Header * tail_FreedObjects[23];
+};
+
+#include "slabsalloc.cpp"
+
+extern "C" {
+  void reportStats();
+  void walk(const std::function< void(Header *) >& f);
+}
+
+#endif
